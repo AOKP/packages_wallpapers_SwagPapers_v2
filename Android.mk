348b7ff4bb00db9f44d69b311b556e7e2f43ac08
remicks <lost.junglist@gmail.com>
2012-07-16 00:56:00 -0500

    Cleanup

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files) $(call all-renderscript-files-under, src)
 
-LOCAL_PACKAGE_NAME := FNVWP
+LOCAL_PACKAGE_NAME := FNVPapers
 LOCAL_CERTIFICATE := shared
 
 include $(BUILD_PACKAGE)
